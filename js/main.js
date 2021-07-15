96c885b6e5674806cb379e87791d12f7223cc671
Hamza <Hamza5955>
2021-07-15 17:00:38 +0200

    base

---
+++ js/main.js
@@ -0,0 +1,4 @@
+/*Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.*/
